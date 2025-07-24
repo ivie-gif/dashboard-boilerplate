bfb61713b1a8393ef0d731a3026e446e40ce9f47
Ivie Maryann Emwiongbon <emwiongbon_ivie@yahoo.com>
2025-07-24 16:48:29 +0100

    feat: changed layout

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard/dashboard"; //import the component as it appears o your file structure
 import Settings from "./pages/SystemSettings/systemSettings"; //import the component as it appears o your file structure
 import Logs from "./pages/ServiceLogs/serviceLogs"; //import the component as it appears o your file structure
-import MainLayout from "./Layout/mainLayout"; //import the component as it appears o your file structure
+import MainLayout from "./layout/mainLayout"; //import the component as it appears o your file structure
 
 const App = () => {
   return (
